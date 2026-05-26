073372fef82a4e9100b74fc78db8d5db499076db
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-27 00:30:52 +0100

    feat: implement tf-atom-dynamodb-table-aws module
    * feat: implement tf-atom-dynamodb-table-aws module
    
    * fix: add full context variable declarations
    
    * fix: regenerate docs and resolve variable conflict
    
    * fix: correct validation reference to dynamodb_attributes

---
+++ main.tf
@@ -0,0 +1,60 @@
+resource "aws_dynamodb_table" "this" {
+  count = local.enabled ? 1 : 0
+
+  name         = module.this.id
+  billing_mode = var.billing_mode
+  hash_key     = var.hash_key
+  range_key    = var.range_key
+
+  dynamic "attribute" {
+    for_each = var.dynamodb_attributes
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
+
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_indexes
+    content {
+      name               = global_secondary_index.value.name
+      hash_key           = global_secondary_index.value.hash_key
+      range_key          = lookup(global_secondary_index.value, "range_key", null)
+      projection_type    = lookup(global_secondary_index.value, "projection_type", "ALL")
+      non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", null)
+      read_capacity      = var.billing_mode == "PROVISIONED" ? lookup(global_secondary_index.value, "read_capacity", 5) : null
+      write_capacity     = var.billing_mode == "PROVISIONED" ? lookup(global_secondary_index.value, "write_capacity", 5) : null
+    }
+  }
+
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_indexes
+    content {
+      name               = local_secondary_index.value.name
+      range_key          = local_secondary_index.value.range_key
+      projection_type    = lookup(local_secondary_index.value, "projection_type", "ALL")
+      non_key_attributes = lookup(local_secondary_index.value, "non_key_attributes", null)
+    }
+  }
+
+  read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
+  write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
+
+  point_in_time_recovery {
+    enabled = var.point_in_time_recovery_enabled
+  }
+
+  server_side_encryption {
+    enabled     = var.server_side_encryption_enabled
+    kms_key_arn = var.kms_key_arn
+  }
+
+  ttl {
+    attribute_name = var.ttl_attribute_name
+    enabled        = var.ttl_enabled
+  }
+
+  deletion_protection_enabled = var.deletion_protection_enabled
+
+  tags = local.tags
+}
